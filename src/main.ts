89316688f306d3b50197cf5276b7e7b1a77d4bbc
b-yp <bypl@vip.qq.com>
2023-11-06 22:57:23 +0800

    fix: Use remark-pangu instead of pangu.js

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import '@logseq/libs'
 import { BlockEntity, IHookEvent } from '@logseq/libs/dist/LSPlugin'
-import pangu from 'pangu'
+
+import { remark } from 'remark'
+import pangu from 'remark-pangu'
 
 import { deepFirstTraversal } from './utils'
 import { logseq as PL } from "../package.json";
+import { isModuleNamespaceObject } from 'util/types'
 
 const pluginId = PL.id;
+
+remark().use(pangu)
 
 const formatPage = async (e: IHookEvent) => {
   const tree = await logseq.Editor.getPageBlocksTree(e.page)
@@ -20,9 +25,10 @@ const formatBlock = async (b?: BlockEntity | IHookEvent) => {
   const block = await logseq.Editor.getBlock(b?.uuid)
   if (!block) return
 
-  const newContent = pangu.spacing(block.content)
-  console.log('>>>', block.content, newContent)
-  logseq.Editor.updateBlock(block.uuid, newContent)
+  remark().use(pangu).process(block.content, (_, file: any) => {
+    // TODO
+    logseq.Editor.updateBlock(block.uuid, String(file).replace(/==\s+/g, '==').replace(/\s+==/g, '=='))
+  });
 }
 
 const main = () => {
